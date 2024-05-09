309b169cff5d7492040def4cffb95c4363fa5997
신용준 <shinyj1991@gmail.com>
2024-05-09 09:31:02 +0900

    feat : 예제 추가

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 
 export default function App() {
   const [show, setShow] = useState<boolean>(false);
+  const [show2, setShow2] = useState<boolean>(true);
 
   return (
     <div>
@@ -22,6 +23,23 @@ export default function App() {
           >
             Modal
             <button type="button" onClick={() => setShow(false)}>
+              Close
+            </button>
+          </div>
+        </Modal>
+      )}
+      {show2 && (
+        <Modal onBackgroundClick={() => setShow2(false)}>
+          <div
+            style={{
+              background: '#fff',
+              height: 1500,
+              width: 768,
+              maxWidth: '100%',
+            }}
+          >
+            Modal2
+            <button type="button" onClick={() => setShow2(false)}>
               Close
             </button>
           </div>
